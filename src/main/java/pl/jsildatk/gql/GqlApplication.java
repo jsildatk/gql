4de5ddd2935a0631ae85d34ce8d81c3a5816baa5
jsildatk <jakub.sildatk@gmail.com>
2020-08-29 18:55:45 +0200

    Initial commit

---
+++ src/main/java/pl/jsildatk/gql/GqlApplication.java
@@ -0,0 +1,13 @@
+package pl.jsildatk.gql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GqlApplication.class, args);
+    }
+    
+}
